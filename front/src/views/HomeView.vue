ec2ff2aa777531377e70714e780a81d5810d4b24
Matheus <matheuspatricksa@gmail.com>
2025-11-05 22:10:09 -0300

    feat: adiciona header

---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <header class="flex flex-col items-center justify-center border-b border-gray-300 py-4">
+      <img src="../../public/logo.png" alt="Logo Santa Casa Saúde" />
+      <p class="text-sm text-gray-400">Sistema de gerenciamento de planos de saúde</p>
+    </header>
+  </div>
+</template>
